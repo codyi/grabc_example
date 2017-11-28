15b05677df3c288fe0e8191b8dcb5d2925d78f14
李国松 <mail@liguosong.com>
2017-11-28 13:36:15 +0800

    一键安装脚本写入数据库的信息

--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
 echo "GoCronJob 一键安装向导"
 
-# echo "安装 beego"
-# go get -u github.com/astaxie/beego
+echo "安装 beego"
+go get -u github.com/astaxie/beego
 
-# echo "安装 bee"
-# go get -u github.com/beego/bee
+echo "安装 bee"
+go get -u github.com/beego/bee
 
-# echo "安装 mysql go-sql-driver"
-# go get -u github.com/go-sql-driver/mysql
+echo "安装 mysql go-sql-driver"
+go get -u github.com/go-sql-driver/mysql
 
-# echo "安装 beego/orm"
-# go get github.com/astaxie/beego/orm
+echo "安装 beego/orm"
+go get github.com/astaxie/beego/orm
 
 echo 
+echo "复制app.conf配置文件"
+cp conf/app.conf.example conf/app.conf
+
+read -p "输入监听端口号(默认8080):" listen_port
+
+if [ -z $listen_port ];then
+	listen_port="8080"
+fi
+echo "\nhttpport = "$listen_port >> conf/app.conf
 echo 
 read -p "确认创建数据库并导入初始化数据(yes or no):" is_install
+
+if [ -z $is_install ];then
+	is_install="no"
+fi
 
 if [ $is_install = "yes" ]; then
 	echo 
@@ -40,6 +53,7 @@ if [ $is_install = "yes" ]; then
 	fi
 
 	mysql -u $user_name -p$passpord -h $host -P $host_port -e "create database if not exists "$database_name" default charset utf8 collate utf8_general_ci;use "$database_name";source conf/install.sql;"
+	echo "\n# 数据库配置\ndb.host = "$host"\ndb.user = "$user_name"\ndb.password = "$passpord"\ndb.port = "$host_port"\ndb.database_name = "$database_name"\n" >> conf/app.conf
 else 
 	echo "数据库创建取消，请按照如下提示手动安装并在数据库中导入conf目录下的install.sql"
 	echo "在conf目录app.conf文件中增加如下配置"
@@ -47,7 +61,6 @@ else
 	printf "# 数据库配置\ndb.host = mysq_host\ndb.user = mysql_user_name\ndb.password = password\ndb.port = port\ndb.name = database_name\n"
 fi
 
-cp conf/app.conf.example conf/app.conf
 echo "\nisInstall = true" >> conf/app.conf
 echo 
 echo "配置完成"
--- models/models.go
+++ models/models.go
@@ -1,12 +1,24 @@
 package models
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	orm.RegisterDataBase("default", "mysql", "root@tcp(127.0.0.1)/goCronJob")
+	db_host := beego.AppConfig.String("db.host")
+	db_user := beego.AppConfig.String("db.user")
+	db_password := beego.AppConfig.String("db.password")
+	db_port := beego.AppConfig.String("db.port")
+	db_name := beego.AppConfig.String("db.database_name")
+
+	if db_password == "" {
+		orm.RegisterDataBase("default", "mysql", db_user+"@tcp("+db_host+":"+db_port+")/"+db_name)
+	} else {
+		orm.RegisterDataBase("default", "mysql", db_user+":"+db_password+"@tcp("+db_host+":"+db_port+")/"+db_name)
+	}
+
 	orm.RegisterModel(new(User))
 }
 
